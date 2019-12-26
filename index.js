b691ab2f9569c14d1e066fd2eefc780f6bc15173
cdr <qualitymanifest@gmail.com>
2019-12-25 20:37:50 -0500

    Add mongoose

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+const keys = require("./config/keys");
+mongoose.connect(
+	keys.mongoURI,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	err => console.log("Mongoose connected", err)
+);
 require("./services/passport");
 const app = express();
 require("./routes/authRoutes")(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
